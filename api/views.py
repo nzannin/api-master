76187248050d22f8a7c15ec88e8a3dbdbb090207
nzannin <nzannin@icloud.com>
2025-06-29 18:44:12 +0200

    unit 9

--- api/views.py
+++ api/views.py
@@ -1,9 +1,7 @@
 from django.db.models import Max
-from django.shortcuts import get_object_or_404
 from api.serializers import ProductSerializer, OrderSerializer, OrderItemSerializer, ProductInfoSerializer
 from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
