ad43cdc3941c53407606e2ee101065e98fd0c691
nzannin <nzannin@icloud.com>
2025-07-06 17:24:36 +0200

    Unit 18

--- api/filters.py
+++ api/filters.py
@@ -1,5 +1,6 @@
 import django_filters
 from api.models import Product
+from rest_framework import filters
 
 class ProductFilter(django_filters.FilterSet):
     class Meta:
@@ -9,3 +10,11 @@ class ProductFilter(django_filters.FilterSet):
             'price': ['exact', 'lt', 'gt', 'lte', 'gte', 'range'],
             'stock': ['exact', 'lt', 'gt', 'lte', 'gte', 'range'],
         }
+      
+        
+class InStockFilterBackend(filters.BaseFilterBackend):
+    """
+    Custom filter to return only products that are in stock.
+    """
+    def filter_queryset(self, request, queryset, view):
+        return queryset.filter(stock__gt=0)
--- api/views.py
+++ api/views.py
@@ -4,8 +4,7 @@ from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
-from rest_framework.views import APIView
-from api.filters import ProductFilter
+from api.filters import ProductFilter, InStockFilterBackend
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -21,7 +20,12 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filterset_class = ProductFilter
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+        InStockFilterBackend
+        ]
     search_fields = ['name', 'description']
     ordering_fields = ['name', 'price', 'stock']
 
